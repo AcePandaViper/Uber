801541e833a33260adadcb45c393a78fa3dcbc0a
DemonNinja91 <85861944+DemonNinja91@users.noreply.github.com>
2022-11-06 21:48:09 +0500

    New text

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 
 const Login = () => {
   return (
     <View>
-      <Text style = {{fontWeight: 'bold'}}> Hellow World  Biches </Text>
+      <Text style = {{fontWeight: 'bold', textAlign: "center", paddingTop: 30, 
+      fontSize: 40, fontFamily: 'monospace', color: 'black'}}> 
+      Uber
+      </Text>
+      <View>
+        <TextInput>
+            
+        </TextInput>
+      </View>
     </View>
   )
 }
