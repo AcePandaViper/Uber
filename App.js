0387b5623e20dfce5c21ba1a3cfe25ce857350b8
DemonNinja91 <85861944+DemonNinja91@users.noreply.github.com>
2022-11-06 21:05:46 +0500

    Init

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { WebView } from 'react-native-webview'
+import Login from './screens/Login'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Login/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ screens/Login.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <View>
+      <Text style = {{fontWeight: 'bold'}}> Hellow World  Biches </Text>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
